eea218e8c1a42e87ca947578523120f66f9d6ed1
Jozhué Beni <jbeeniif@gmail.com>
2023-09-04 16:04:47 -0300

    Base of development of MOttaMoveis

---
+++ motta-moveis/src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <>começando a aplicação</>;
+}
+
+export default App;
---
+++ motta-moveis/src/components/header/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const headerHome = () => {
+  return <div></div>;
+};
+
+export default headerHome;
