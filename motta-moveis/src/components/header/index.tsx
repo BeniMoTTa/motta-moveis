b8887005a14847702015776620a463f90699fabb
Jozhué Beni <jbeeniif@gmail.com>
2023-09-05 16:23:33 -0300

    Logo Motta

--- motta-moveis/src/components/header/index.tsx
+++ motta-moveis/src/components/header/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import logo from "../../assets/LogoMottaImoveis.png";
 const Header = () => {
   return (
-    <header className=" w-full h-[120px] flex bg-[yellow] flex items-center">
-      {" "}
-      testando
+    <header className=" w-full h-[120px] flex bg-[yellow]">
+      <div className="w-[95%] allign px-[2%] h-full bg-[black] text-[pink] flex items-center">
+        <img src={logo} alt="" className="w-[160px]" />
+        {}
+        testando
+      </div>
     </header>
   );
 };
