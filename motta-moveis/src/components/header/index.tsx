f7f21cf6b594666fb801d92f676e284982529b60
Jozhué Beni <jbeeniif@gmail.com>
2023-09-06 15:59:53 -0300

    some fix bugs

--- motta-moveis/src/components/header/index.tsx
+++ motta-moveis/src/components/header/index.tsx
@@ -3,11 +3,24 @@ import logo from "../../assets/LogoMottaImoveis.png";
 const Header = () => {
   return (
     <header className=" w-full h-[120px] flex bg-[yellow]">
-      <div className="w-[95%] allign px-[2%] h-full bg-[black] text-[pink] flex items-center">
+      <div className="w-[95%] allign px-[2%] h-full bg-[black] text-[pink] flex items-center justify-between">
         <img src={logo} alt="" className="w-[160px]" />
         {}
-        testando
+        <div className="gap-[2%] flex">
+          <button className="basic-btn">Comprar</button>
+          <button className="basic-btn">Alugar</button>
+          <button className="basic-btn">Lançamentos</button>
+          <button className="basic-btn">Descobrir</button>
+          <button className="basic-btn">Financiamento</button>
+          <button className="basic-btn">Ajuda</button>
+          <button className="basic-btn">Anuncie conosco</button>
+          <button className="basic-btn">Criar conta</button>
+        </div>
       </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </header>
   );
 };
