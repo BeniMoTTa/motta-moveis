a137a3dda1e37f66b95cafb4ddf6634ecae81506
Jozhué Beni <jbeeniif@gmail.com>
2023-09-15 16:50:19 -0300

    Finished Register page with animations and button to back

--- motta-moveis/src/App.tsx
+++ motta-moveis/src/App.tsx
@@ -1,5 +1,7 @@
 import "./index.css";
 import "./styles/cards.css";
+import "./styles/buttons.css";
+
 import { RoutesMain } from "./routes";
 function App() {
   return (
--- motta-moveis/src/pages/register/Register.tsx
+++ motta-moveis/src/pages/register/Register.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "../../assets/LogoMottaImoveis.png";
 import { BsFacebook } from "react-icons/bs";
 import { BiLogoGmail } from "react-icons/bi";
+import { SlLogout } from "react-icons/sl";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
@@ -53,7 +55,7 @@ const Register = () => {
                 type="text"
                 id="nome"
                 name="nome"
-                className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
+                className="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
                 placeholder="Escreva aqui seu nome"
               />
             </div>
@@ -69,7 +71,7 @@ const Register = () => {
                 type="text"
                 id="sobrenome"
                 name="sobrenome"
-                className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
+                className="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
                 placeholder="Escreva aqui seu sobrenome..."
               />
             </div>
@@ -85,7 +87,7 @@ const Register = () => {
                 type="email"
                 id="email"
                 name="email"
-                className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
+                className="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
                 placeholder="Escreva aqui seu email..."
               />
             </div>
@@ -101,7 +103,7 @@ const Register = () => {
                 type="password"
                 id="senha"
                 name="senha"
-                className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
+                className="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
                 placeholder="Escreva aqui sua senha..."
               />
             </div>
@@ -117,20 +119,30 @@ const Register = () => {
                 type="tel"
                 id="telefone"
                 name="telefone"
-                className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
+                className="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500"
                 placeholder="Escreva aqui seu número..."
               />
             </div>
-            <button className="w-full h-14 text-white mt-[20px] rounded-3xl flex justify-center items-center bg-gradient-to-r from-orange-400 to-red-500 gap-2 text-[21px] font-bold">
-              Sign up
+            <button className=" custom-btn btn-12  text-white mt-[20px] rounded-3xl flex justify-center items-center relative overflow-hidden text-[21px] font-bold">
+              <span>Click!</span>
+              <span>Sign up</span>
             </button>
-            <div className="w-full flex justify-center mt-[10px]">
+            <div className="w-full flex justify-center items-center mt-[10px] flex-col">
               <p className="text-[18px]">
                 Já possui conta?{" "}
                 <span className="text-[orange] hover:underline cursor-pointer">
                   Clique aqui!
                 </span>
               </p>
+              <Link
+                to={"/"}
+                className="hover:text-[orange] hover:font-bold cursor-pointer flex justify-center items-center gap-2 font-bold underline link mt-2"
+              >
+                <span className="text-[18px] span-animation">
+                  <SlLogout />
+                </span>
+                Voltar para a home
+              </Link>
             </div>
           </form>
         </div>
