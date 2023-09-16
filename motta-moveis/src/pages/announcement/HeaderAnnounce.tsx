6a3190bf9ea6b1998e8c323ec180e2c25f64a2bf
Jozhué Beni <jbeeniif@gmail.com>
2023-09-16 11:06:12 -0300

    Header announcement initiate

--- motta-moveis/src/pages/announcement/HeaderAnnounce.tsx
+++ motta-moveis/src/pages/announcement/HeaderAnnounce.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { FaRegUserCircle } from "react-icons/fa";
 import logo from "../../assets/LogoMottaImoveis.png";
 
 const HeaderAnnounce = () => {
   return (
-    <header className="w-full bg-[orange-300]">
-      <div className="max-w-[1400px]">
-        <img src={logo} alt="" />
+    <header className="w-full bg-[orange-300] h-20  ">
+      <div className="max-w-[1400px] flex allign items-center h-full justify-between">
+        <img src={logo} alt="" className="w-[180px]" />
+        <button className="flex gap-3 border-2 w-24 h-10 justify-center rounded items-center text-[18px] text-grey3">
+          <span className="text-[24px] text-grey3">
+            <FaRegUserCircle />
+          </span>
+          Entrar
+        </button>
       </div>
     </header>
   );
